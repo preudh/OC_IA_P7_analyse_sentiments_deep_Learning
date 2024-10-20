488b09a56eeb49f4c859c115bc3e79f57298e73f
Patrice Preudhomme <preudhomme.patrice@gmail.com>
2024-10-20 12:38:29 +0200

    file mlrun for all models

--- launch_mlflow.py
+++ launch_mlflow.py
@@ -1,9 +1,26 @@
+# from pathlib import Path
+# import os
+#
+# def launch_mlflow_server():
+#     # Chemin absolu vers le dossier "mlruns" à la racine du projet
+#     mlruns_path = Path("../mlruns").resolve()
+#
+#     # Vérification que le dossier "mlruns" existe, sinon le créer
+#     if not mlruns_path.exists():
+#         print(f"Le dossier {mlruns_path} n'existe pas. Création du dossier...")
+#         mlruns_path.mkdir(parents=True)
+#
+#     # Lancer le serveur MLflow avec le bon chemin
+#     os.system(f"mlflow ui --backend-store-uri {mlruns_path.as_uri()} --host 0.0.0.0 --port 5000")
+#
+# if __name__ == "__main__":
+#     launch_mlflow_server()
 from pathlib import Path
 import os
 
 def launch_mlflow_server():
     # Chemin absolu vers le dossier "mlruns" à la racine du projet
-    mlruns_path = Path("../mlruns").resolve()
+    mlruns_path = Path("./mlruns").resolve()
 
     # Vérification que le dossier "mlruns" existe, sinon le créer
     if not mlruns_path.exists():
@@ -11,7 +28,7 @@ def launch_mlflow_server():
         mlruns_path.mkdir(parents=True)
 
     # Lancer le serveur MLflow avec le bon chemin
-    os.system(f"mlflow ui --backend-store-uri {mlruns_path.as_uri()} --host 0.0.0.0 --port 5000")
+    os.system(f"mlflow ui --backend-store-uri {mlruns_path.as_uri()} --host 127.0.0.1 --port 5000")
 
 if __name__ == "__main__":
     launch_mlflow_server()
